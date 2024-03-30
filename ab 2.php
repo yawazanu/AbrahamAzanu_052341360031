81c45c3a415ff9464fd24aeb9affed641170ceb9
yawazanu <yawazanu@gmail.com>
2024-03-29 20:06:08 -0700

    website about faculty of applied science

---
+++ ab 2.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "123456789";
+$dbname = "facul_app_sci";
+
+// Create connection
+$conn =new mysqli($servername, $username, $password, $database);
+
+echo"database is connect successful! THANK YOU!!!";
